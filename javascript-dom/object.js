8acbb8ff331c7a122c7cb16e046a465fe64c21a3
githubna-ilham <ilhamnazhief@gmail.com>
2024-01-30 09:02:05 +0700

    object-dom

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -1,13 +1,21 @@
 var siswa = new Object();
-siswa.namaDepan = "nazhief";
-siswa.namaBelakang = "ilham";
-siswa.alamat = "bandung";
+siswa.namaDepan = "Nazhief";
+siswa.namaBelakang = "Ilham";
+siswa.alamat = "Bandung";
 siswa.namaLengkap = function() {
     return this.namaDepan + " " + this.namaBelakang;
 };
 
+var siswa2 = {
+    namaDepan : "Akzal",
+    namaBelakang : "Damusti",
+    alamat : "Bandung",
+    namaLengkap : function() {
+        return this.namaDepan + " " + this.namaBelakang;
+    },
+};
 alert("Nama : " + siswa.namaLengkap());
-
+alert("Nama : " + siswa2.namaLengkap());
 //function namaLengkap() {
 
 // // }
